42fbb9372c10ec80947f14a64fd90268b52bab1a
gembox <rhelenius@gmail.com>
2015-11-10 06:06:49 -0500

    Add formatted biography information

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,4 +2,34 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
- $("main").append("Ross Helenius")
+
+var skills = ["HTML","SQL","Javascript","Analytics"]
+
+var bio = {
+	"name" : "Ross Helenius",
+	"role" : "Web Developer",
+	"contact-info" : "rhelenius@gmail.com",
+	"picture" : "https://lh3.googleusercontent.com/-sd8iEO5bxA4/VjS7TK4XNiI/AAAAAAAAAJU/NBqAoL_PRL0/w140-h140-p/MePic.PNGzoom=2&amp;resize=300%2C300",
+	"welcome" : "Welcome to my page about me.",
+	"skills" : skills
+
+var formattedName =HTMLheaderName.replace("%data%", name);
+
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+var formattedContact = HTMLcontactGeneric.replace("%data%", contact-info);
+
+var formattedPicture = HTMLbioPic.replace("%data%", picture);
+
+var formattedWelcome = HTMLwelcomeMsg.replace("%data%", welcome);
+
+var formattedSkills = HTMLskills.replace("%data", skills);
+
+
+
+
+};
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#header").append()
