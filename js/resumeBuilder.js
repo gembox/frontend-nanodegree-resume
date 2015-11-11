a09b285f0a5026caf88dc21d467aae06dccc9f3a
gembox <rhelenius@gmail.com>
2015-11-11 08:39:18 -0500

    Fix contact information formatting

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,34 +2,52 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
-
-var skills = ["HTML","SQL","Javascript","Analytics"]
+//JSON Objects
+var skills = ["HTML","SQL","Javascript","Analytics"];
 
 var bio = {
 	"name" : "Ross Helenius",
 	"role" : "Web Developer",
-	"contact-info" : "rhelenius@gmail.com",
+	"contact" : {
+						"mobile" : "978-226-8509",
+						"email" : "rhelenius@gmail.com",
+						"github" : "gembox",
+						"twitter" : "rhelenius118",
+						"location" : "Boston"
+					},
 	"picture" : "https://lh3.googleusercontent.com/-sd8iEO5bxA4/VjS7TK4XNiI/AAAAAAAAAJU/NBqAoL_PRL0/w140-h140-p/MePic.PNGzoom=2&amp;resize=300%2C300",
 	"welcome" : "Welcome to my page about me.",
 	"skills" : skills
-
-var formattedName =HTMLheaderName.replace("%data%", name);
-
-var formattedRole = HTMLheaderRole.replace("%data%", role);
-
-var formattedContact = HTMLcontactGeneric.replace("%data%", contact-info);
-
-var formattedPicture = HTMLbioPic.replace("%data%", picture);
-
-var formattedWelcome = HTMLwelcomeMsg.replace("%data%", welcome);
-
-var formattedSkills = HTMLskills.replace("%data", skills);
-
-
-
-
 };
 
+
+//Use the formatting from helper.js
+var formattedName =HTMLheaderName.replace("%data%", bio.name);
+
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+
+var formattedMobile = HTMLmobile.replace("%data%", bio.contact.mobile);
+
+var formattedEmail = HTMLemail.replace("%data%",bio.contact.email);
+
+var formattedGithub = HTMLgithub.replace("%data%",bio.contact.github);
+
+var formattedTwitter = HTMLtwitter.replace("%data%",bio.contact.twitter);
+
+var formattedLocation = HTMLlocation.replace("%data%",bio.contact.location);
+
+var formattedPicture = HTMLbioPic.replace("%data%", bio.picture);
+
+var formattedWelcome = HTMLwelcomeMsg.replace("%data%", bio.welcome);
+
+var formattedSkills = HTMLskills.replace("%data", bio.skills);
+
+//position based off index.html
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
-$("#header").append()
+$("#topContacts").append(formattedMobile);
+$("#topContacts").append(formattedEmail);
+$("#topContacts").append(formattedGithub);
+$("#topContacts").append(formattedTwitter);
+$("#topContacts").append(formattedLocation);
+$("#main").append(formattedPicture);
