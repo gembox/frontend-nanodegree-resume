e779f176f0b8bef79c1b476b47fd927313377919
gembox <rhelenius@gmail.com>
2015-11-22 20:52:13 -0500

    Clean up JSON formmatting and add online courses to education

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,85 +5,94 @@ This is empty on purpose! Your code to build the resume will go here.
 //JSON Objects
 //object to hold the bio information
 var bio = {
-  "name" : "Ross Helenius",
-   "role" : "Web Developer",
-   "contact" : {
-                 "mobile" : "978-226-8509",
-                 "email" : "rhelenius@gmail.com",
-                 "github" : "gembox",
-                 "twitter" : "rhelenius118",
-                 "location" : ["Boston, MA"]
-               },
-   "picture" : "https://lh3.googleusercontent.com/-sd8iEO5bxA4/VjS7TK4XNiI/AAAAAAAAAJU/NBqAoL_PRL0/w140-h140-p/MePic.PNGzoom=2&amp;resize=300%2C300",
-   "welcome" : "Welcome to my page about me.",
-   "skills" : ["HTML","Javascript","SQL","Analytics"]
+  "name": "Ross Helenius",
+  "role": "Web Developer",
+  "contact": {
+    "mobile": "978-226-8509",
+    "email": "rhelenius@gmail.com",
+    "github": "gembox",
+    "twitter": "rhelenius118",
+    "location": ["Boston, MA"]
+  },
+  "biopic": "https://lh3.googleusercontent.com/-sd8iEO5bxA4/VjS7TK4XNiI/AAAAAAAAAJU/NBqAoL_PRL0/w140-h140-p/MePic.PNGzoom=2&amp;resize=300%2C300",
+  "welcomeMessage": "Welcome to my page about me.",
+  "skills": ["HTML", "Javascript", "SQL", "Analytics"]
 };
 
 //object to hold the work information
 var work = {
-  "jobs" : [
-       {
-        "employer" : "athenahealth",
-        "title" :  "Data Solutions Manager",
-        "location" : "Watertown, MA",
-        "years" : "2014-Current",
-        "description" : "Manage custom report requests."
-        },
-        {
-        "employer" : "UMass Memorial",
-        "title" :  "Applications/Systems Specialist",
-        "location" : "Worcester, MA",
-        "years" : "2012-2014",
-        "description" : "Perform custom report and interface work."
-        },
-        {
-        "employer" : "Alere",
-        "title" :  "Solutions Analyst",
-        "location" : "Waltham, MA",
-        "years" : "2012",
-        "description" : "Manage features of the application for the windows platform."
-        },
-        {
-        "employer" : "TEK Systems",
-        "title" :  "Implementation Consultant",
-        "location" : "Nashville, TN",
-        "years" : "2011-2012",
-        "description" : "Consult on the Allscripts EHR rollout."
-        },
-        {
-        "employer" : "Lahey Clinic",
-        "title" :  "Systems Analyst III",
-        "location" : "Burlington, MA",
-        "years" : "2009-2011",
-        "description" : "Perform various functions on the Allscripts EHR rollout."
-        }
-    ]
+  "jobs": [{
+    "employer": "athenahealth",
+    "title": "Data Solutions Manager",
+    "location": "Watertown, MA",
+    "dates": "2014-Current",
+    "description": "Manage custom report requests."
+  },
+  {
+    "employer": "UMass Memorial",
+    "title": "Applications/Systems Specialist",
+    "location": "Worcester, MA",
+    "dates": "2012-2014",
+    "description": "Perform custom report and interface work."
+  },
+  {
+    "employer": "Alere",
+    "title": "Solutions Analyst",
+    "location": "Waltham, MA",
+    "dates": "2012",
+    "description": "Manage features of the application for the windows platform."
+  },
+  {
+    "employer": "TEK Systems",
+    "title": "Implementation Consultant",
+    "location": "Nashville, TN",
+    "dates": "2011-2012",
+    "description": "Consult on the Allscripts EHR rollout."
+  },
+  {
+    "employer": "Lahey Clinic",
+    "title": "Systems Analyst III",
+    "location": "Burlington, MA",
+    "dates": "2009-2011",
+    "description": "Perform various functions on the Allscripts EHR rollout."
+  }]
 };
 
 //object to hold the education information
 var education = {
-  "schools" : [
-    {
+  "schools" : [{
     "name" : "UMass Lowell",
-    "gradyear" : "2010",
+    "dates" : "2010",
     "location" : "Lowell, MA",
     "degree" : "Certificate",
-    "major" : "Information Technology"
+    "major" : "Information Technology",
+    "url" : "http://www.uml.edu"
     },
     {
     "name" : "Rollins College",
-    "gradyear" : "2005",
+    "dates" : "2005",
     "location" : "Winter Park, FL",
     "degree" : "BA",
-    "major" : "Psychology"
-    }
-  ]
+    "major" : "Psychology",
+    "url" : "http://www.rollins.edu"
+    }],
+  "onlineCourses" : [{
+    "title" : "UNIX",
+    "school" : "UML",
+    "date" : "2009",
+    "url" : "http://www.uml.edu"
+    },
+    {
+    "title" : "C Programming",
+    "school" : "UML",
+    "date" : "2009",
+    "url" : "http://www.uml.edu"
+  }]
 };
 
-//object to hodl the project information
-var project = {
-  "projects" : [
-    {
+//object to hold the project information
+var projects = {
+  "projects" : [{
     "title" : "Test Title",
     "dates" : "2015",
     "description" : "Some text here.",
@@ -94,8 +103,7 @@ var project = {
     "dates" : "2015",
     "description" : "Some text here.2",
     "images" : ["http://placehold.it/100x100"]
-    }
-  ]
+    }]
 };
 
 //add the bio section to the top of the page and contacts to the footer
@@ -106,10 +114,10 @@ bio.display = function() {
   var formattedName =HTMLheaderName.replace("%data%", bio.name);
   $("#header").prepend(formattedName);
 
-  var formattedPicture = HTMLbioPic.replace("%data%", bio.picture);
+  var formattedPicture = HTMLbioPic.replace("%data%", bio.biopic);
   $("#header").append(formattedPicture);
 
-  var formattedWelcome = HTMLwelcomeMsg.replace("%data%", bio.welcome);
+  var formattedWelcome = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
   $("#header").append(formattedWelcome);
 
   var formattedMobile = HTMLmobile.replace("%data%", bio.contact.mobile);
@@ -155,7 +163,7 @@ work.display = function() {
       var formattedTitleandEmployer = formattedEmployer + formattedTitle;
       $(".work-entry:last").append(formattedTitleandEmployer);
 
-      var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].years);
+      var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
       $(".work-entry:last").append(formattedDates);
 
       var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
@@ -168,20 +176,20 @@ work.display();
 
 //show the project section of the resume
 projects.display = function() {
-  for (var item in project.projects) {
+  for (var item in projects.projects) {
     $("#projects").append(HTMLprojectStart);
 
-    if (project.projects.hasOwnProperty(item)) {
-      var formattedTitle = HTMLprojectTitle.replace("%data%", project.projects[item].title);
+    if (projects.projects.hasOwnProperty(item)) {
+      var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[item].title);
       $(".project-entry:last").append(formattedTitle);
 
-      var formattedDates = HTMLprojectDates.replace("%data%", project.projects[item].dates);
+      var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[item].dates);
       $(".project-entry:last").append(formattedDates);
 
-      var formattedDescription = HTMLprojectDescription.replace("%data%", project.projects[item].description);
+      var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[item].description);
       $(".project-entry:last").append(formattedDescription);
 
-      var formattedImage = HTMLprojectImage.replace("%data%", project.projects[item].images);
+      var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[item].images);
       $(".project-entry:last").append(formattedImage);
     }
   }
@@ -198,7 +206,7 @@ education.display = function() {
       var formattedName = HTMLschoolName.replace("%data%", education.schools[school].name);
       $(".education-entry:last").append(formattedName);
 
-      var formattedDates = HTMLschoolDates.replace("%data%", education.schools[school].gradyear);
+      var formattedDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
       $(".education-entry:last").append(formattedDates);
 
       var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
@@ -210,7 +218,26 @@ education.display = function() {
       var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
       $(".education-entry:last").append(formattedDegree);
     }
-  }
+  };
+
+  $("#education").append(HTMLonlineClasses);
+
+  for (var course in education.onlineCourses) {
+    $("#education").append(HTMLonlineStart);
+
+    if (education.onlineCourses.hasOwnProperty(course)) {
+
+      var formattedClass = HTMLonlineTitle.replace("%data%", education.onlineCourses[course].title);
+      var formattedSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[course].school);
+      $(".online-entry:last").append(formattedClass + formattedSchool);
+
+      var formattedDate = HTMLonlineDates.replace("%data%", education.onlineCourses[course].date);
+      $(".online-entry:last").append(formattedDate);
+
+      var formattedURL = HTMLonlineURL.replace("%data%", education.onlineCourses[course].url);
+      $(".online-entry:last").append(formattedURL);
+    }
+  };
 };
 
 education.display();
